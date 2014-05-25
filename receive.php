8129958c473a68c8a7cd22e7b9f5c1639f357722
Philipp Hagenlocher <P.Hagenlocher@hotmail.de>
2014-05-25 14:56:56 +0200

    Update receive.php
    Saving the file under the IP instead of computername

--- receive.php
+++ receive.php
@@ -1,16 +1,34 @@
+
 <?php
 
-$name = $_GET["name"];
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
 $content = $_GET["content"];
+$ip = get_client_ip();
 
 // If the file already exist, we don't need the <h1> title anymore.
-if(file_exists($name))
+if(file_exists($ip))
 {
 	$pos = strrpos($content, "</h1>");
 	$content = substr($content, $pos+5, strlen($content) - $pos);
 }
 
-$file = fopen($name,"a+");
+$file = fopen($ip,"a+");
 fwrite($file, $content);
 fclose($file);
 
