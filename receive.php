83c12651666a8e35b83946a95e6c635ec5f6de1b
Philipp Hagenlocher <P.Hagenlocher@hotmail.de>
2014-05-25 18:41:08 +0200

    Update receive.php

--- receive.php
+++ receive.php
@@ -20,14 +20,6 @@ function get_client_ip() {
 
 $content = $_GET["content"];
 $ip = get_client_ip();
-
-// If the file already exist, we don't need the <h1> title anymore.
-if(file_exists($ip))
-{
-	$pos = strrpos($content, "</h1>");
-	$content = substr($content, $pos+5, strlen($content) - $pos);
-}
-
 $file = fopen($ip,"a+");
 fwrite($file, $content);
 fclose($file);
